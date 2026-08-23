6add89f1a18e90f4d54928df3be75a477184cb32
Tomas Saint Avit <tomas.saintavit@gmail.com>
2026-08-23 19:43:51 -0300

    test(db): aislar tests en base ipona_test

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,3 +1,4 @@
 import os
 
 os.environ["SCHEDULER_ENABLED"] = "false"
+os.environ["DATABASE_URL"] = "postgresql+asyncpg://ipona:ipona_dev@localhost:5432/ipona_test"
---
+++ docker/initdb/create-test-db.sql
@@ -0,0 +1,2 @@
+-- Se ejecuta solo en la inicialización de un volumen nuevo de Docker.
+CREATE DATABASE ipona_test;
